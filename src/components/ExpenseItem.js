4ba506a3a8ad015696066135caf2a3730b230e46
Varsha0095 <varsha03prajapati@gmail.com>
2022-11-08 12:45:37 +0530

    locationOfExpenditureAdded

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
-function ExpenseItem() {
+
+import './ExpenseItem.css'      
+
+function ExpenseItem(props) {
+
   return (
-    <div>
-      <div>Food-Rs.10</div>
-      <div>Petrol-Rs.100</div>
-      <div>Movies-Rs.200</div>
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'></div>
+      <h2>{props.title}</h2>
+      <h2>{props.locationOfExpenditure}</h2>
+      <div className='expense-item__price'>${props.amount}</div>
     </div>
   );
 }
