0a4776cf27f5dd75ee76e9a9f7360a1e32995fd1
Varsha0095 <varsha03prajapati@gmail.com>
2022-11-11 22:07:49 +0530

    AddEventOnForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -35,36 +36,11 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
-
-      {/* <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        locationOfExpenditure={expenses[0].locationOfExpenditure}
-      ></ExpenseItem>       
       
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        locationOfExpenditure={expenses[1].locationOfExpenditure}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        locationOfExpenditure={expenses[2].locationOfExpenditure}
-    ></ExpenseItem>
-    <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        locationOfExpenditure={expenses[3].locationOfExpenditure}
-    ></ExpenseItem>  */}
     </div>
   );
-}
+};
 
 export default App;
