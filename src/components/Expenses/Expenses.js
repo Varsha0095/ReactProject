381fada2e4febde6b64a0e700bc2d7baa7a90753
Varsha0095 <varsha03prajapati@gmail.com>
2022-11-13 22:27:48 +0530

    ExpenseFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,46 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
+import "./Expenses.css";
 
 const Expenses = (props) => {
-    return (<Card className="expenses">
-        <ExpenseItem 
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-        locationOfExpenditure={props.items[0].locationOfExpenditure}
-        id={props.items[0].id}
+  const[filteredYear, setFilteredYear] = useState('2020')
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    // console.log('Expense.js');
+    // console.log(selectedYear);
+  }
+  return (
+    <div>
+      <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
 
-      ></ExpenseItem>
-      <ExpenseItem 
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        locationOfExpenditure={props.items[1].locationOfExpenditure}
-        id={props.items[1].id}
-
-      ></ExpenseItem>
-      <ExpenseItem 
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        locationOfExpenditure={props.items[2].locationOfExpenditure}
-        id={props.items[2].id}
-
-      ></ExpenseItem>
-      <ExpenseItem 
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        locationOfExpenditure={props.items[3].locationOfExpenditure}
-        id={props.items[3].id}
-
-
-      ></ExpenseItem>
-    </Card>
-    )
-}
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+          locationOfExpenditure={props.items[0].locationOfExpenditure}
+          id={props.items[0].id}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+          locationOfExpenditure={props.items[1].locationOfExpenditure}
+          id={props.items[1].id}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+          locationOfExpenditure={props.items[2].locationOfExpenditure}
+          id={props.items[2].id}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+          locationOfExpenditure={props.items[3].locationOfExpenditure}
+          id={props.items[3].id}
+        ></ExpenseItem>
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
